a0fcffb914976e2c410d3e04db11c9a620bc346e
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-05-18 21:08:03 +0200

    Add bottom-nav to control pager

--- flutter_random_images/lib/image_app_parger.dart
+++ flutter_random_images/lib/image_app_parger.dart
@@ -12,6 +12,16 @@ class ImageAppPager extends StatefulWidget {
 }
 
 class _ImageAppPagerState extends State<ImageAppPager> {
+  int _selectedIndex = 0;
+  PageView _pageView = null;
+
+  PageView _initPageView() {
+    _pageView = PageView.builder(
+        itemBuilder: (BuildContext context, int index) =>
+            ImageAppPage(index: index));
+    return _pageView;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +30,55 @@ class _ImageAppPagerState extends State<ImageAppPager> {
         elevation: 15,
         title: Text(widget._title),
       ),
-      body: PageView.builder(
-          itemBuilder: (BuildContext context, int index) =>
-              ImageAppPage(index: index)),
+      body: _initPageView(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.arrow_left),
+            title: Text("Backward"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.arrow_right),
+            title: Text("Forward"),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+
+      if (index == 0) {
+        // For the 0.page(first page), ignore.
+        if (_pageView.controller.page < 1) {
+          debugPrint("to page, ignore on first page");
+          return;
+        }
+        _backwardPager(Duration(milliseconds: 200), SawTooth(5));
+      } else {
+        _forwardPager(Duration(milliseconds: 200), SawTooth(5));
+      }
+    });
+  }
+
+  void _forwardPager(Duration duration, Curve curve) {
+    int toPage = (_pageView.controller.page + 1.0).toInt();
+
+    _pageView.controller.nextPage(duration: duration, curve: curve);
+
+    debugPrint("to page $toPage");
+  }
+
+  void _backwardPager(Duration duration, Curve curve) {
+    int toPage = (_pageView.controller.page - 1.0).toInt();
+
+    _pageView.controller.previousPage(duration: duration, curve: curve);
+
+    debugPrint("to page $toPage");
   }
 }
