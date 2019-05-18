7c19f47683bc98010845cdb1c0b5f5c6f031c0e0
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-05-18 12:14:05 +0200

    Rename file image_page -> image_app_page

--- flutter_random_images/lib/main.dart
+++ flutter_random_images/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_random_images/image_page.dart';
+import 'package:flutter_random_images/image_app_page.dart';
 
 void main() => runApp(MyApp());
 
