6fefba9e2b0d14bffd4e444c95ec2de906bf3a09
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-05-20 01:11:45 +0200

    Start tests for ping

--- flutter_random_images/lib/image_app_splash.dart
+++ flutter_random_images/lib/image_app_splash.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_random_images/domain/Ping.dart';
 import 'package:flutter_random_images/image_app_pager.dart';
 import 'package:flutter_random_images/viewmodel/image_app_splash_view_model.dart';
 
