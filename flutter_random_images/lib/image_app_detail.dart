48de8fb498541976b43b76ce7cd4038ea97a7c3e
ChrisDEV <7869833+XinyueZ@users.noreply.github.com>
2019-05-24 19:52:07 +0200

    Optimize/flutter random images photo detail (#4)

--- flutter_random_images/lib/image_app_detail.dart
+++ flutter_random_images/lib/image_app_detail.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 import 'package:flutter_random_images/domain/photo.dart';
 
 import 'config.dart';
@@ -9,20 +10,93 @@ class ImageAppDetail extends StatelessWidget {
 
   ImageAppDetail(this._photo);
 
+  void _showPhotoInformation(context) {
+    showModalBottomSheet<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.people),
+                title: Text(_photo.author),
+              ),
+              ListTile(
+                leading: Icon(Icons.format_size),
+                title: Text("${_photo.width} x ${_photo.height}"),
+              ),
+              ListTile(
+                leading: Icon(Icons.web),
+                title: InkWell(
+                    child: Text("${_photo.webLocation}",
+                        style: TextStyle(
+                            color: Colors.lightBlue,
+                            decoration: TextDecoration.underline)),
+                    onTap: () => _launchURL(context, _photo.webLocation)),
+              ),
+            ],
+          );
+        });
+  }
+
+  void _launchURL(BuildContext context, Uri target) async {
+    try {
+      await launch(
+        target.toString(),
+        option: CustomTabsOption(
+          toolbarColor: Colors.black,
+          //Theme.of(context).primaryColor,
+          enableDefaultShare: true,
+          enableUrlBarHiding: true,
+          showPageTitle: true,
+          animation: CustomTabsAnimation.slideIn(),
+          extraCustomTabs: <String>[
+            'org.mozilla.firefox',
+            'com.microsoft.emmx',
+          ],
+        ),
+      );
+    } catch (e) {
+      // An exception is thrown if browser app is not installed on Android device.
+      debugPrint(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: CachedNetworkImage(
-      placeholder: (context, url) => Image.asset(
-            placeholderUri,
-            fit: BoxFit.cover,
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        CachedNetworkImage(
+          placeholder: (context, url) => Image.asset(
+                placeholderUri,
+                fit: BoxFit.cover,
+              ),
+          errorWidget: (context, url, error) => Image.asset(
+                errorUri,
+                fit: BoxFit.cover,
+              ),
+          imageUrl: _photo.downloadUrl.toString(),
+          fit: BoxFit.cover,
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text(_photo.author),
+            backgroundColor: Colors.transparent,
           ),
-      errorWidget: (context, url, error) => Image.asset(
-            errorUri,
-            fit: BoxFit.cover,
+          backgroundColor: Colors.transparent,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              _showPhotoInformation(context);
+            },
+            backgroundColor: Colors.pinkAccent,
+            child: Icon(
+              Icons.info,
+              color: Colors.white,
+            ),
           ),
-      imageUrl: _photo.downloadUrl.toString(),
-      fit: BoxFit.cover,
-    ));
+        ),
+      ],
+    );
   }
 }
