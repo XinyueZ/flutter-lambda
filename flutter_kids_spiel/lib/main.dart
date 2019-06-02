5f607d184c689f0652dd6790ad7042ac31d30370
ChrisDEV <7869833+XinyueZ@users.noreply.github.com>
2019-06-02 17:48:06 +0200

    Add project flutter_kids_spiel (#10)
    - Add location permission (#12)(#28)
    - [PRE-RELEASE] v0.0.1+1 (#26)
    - Add app icon (#27)
    - API connection (#29)
    - Add pin for grounds (#30)
    - Click pin of ground and navi (#31)
    - Show new pin, new launcher, improve code struct and goto pin etc(#32)
    
    flutter_kids_spiel: Bigger pin bit

---
+++ flutter_kids_spiel/lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'map.dart';
+
+void main() => runApp(App());
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final navigatorKey = GlobalKey<NavigatorState>();
+  final map = MapView();
+  Color _fabColor;
+  PermissionStatus _permissionStatus = PermissionStatus.unknown;
+
+  @override
+  void initState() {
+    PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.location)
+        .then((PermissionStatus status) {
+      setState(() {
+        _permissionStatus = status;
+        _fabColor = _getPermissionColor();
+
+        _syncMapCamera();
+      });
+    });
+    PermissionHandler()
+        .shouldShowRequestPermissionRationale(PermissionGroup.location)
+        .then((isShown) {
+      if (!isShown) _requestPermission(false);
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      title: 'Kids playground search',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: Scaffold(
+          body: map,
+          floatingActionButton: Padding(
+            padding: EdgeInsets.only(bottom: 24),
+            child: FloatingActionButton(
+              onPressed: () async {
+                await _requestPermission(true);
+                await _moveMapCamera();
+              },
+              child: Icon(Icons.my_location),
+              backgroundColor: _fabColor,
+            ),
+          )),
+    );
+  }
+
+  Future _moveMapCamera() async {
+    if (_permissionStatus == PermissionStatus.granted) {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      map.center = position;
+    }
+  }
+
+  void _syncMapCamera() {
+    if (_permissionStatus == PermissionStatus.granted) {
+      Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+          .then((position) {
+        map.center = position;
+      });
+    }
+  }
+
+  Color _getPermissionColor() {
+    switch (_permissionStatus) {
+      case PermissionStatus.granted:
+        return Colors.pinkAccent;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  Future<void> _requestPermission(bool showRationale,
+      {PermissionGroup permission = PermissionGroup.location}) async {
+    final List<PermissionGroup> permissions = <PermissionGroup>[permission];
+    final Map<PermissionGroup, PermissionStatus> permissionRequestResult =
+        await PermissionHandler().requestPermissions(permissions);
+
+    setState(() {
+      _permissionStatus = permissionRequestResult[permission];
+      _fabColor = _getPermissionColor();
+    });
+
+    if (showRationale && _permissionStatus != PermissionStatus.granted) {
+      _showPermissionRationale();
+    }
+
+    _syncMapCamera();
+  }
+
+  void _showPermissionRationale() {
+    showDialog(
+        context: navigatorKey.currentState.overlay.context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Warning"),
+            content: Text(
+                "App needs location permission, please turn on it, otherwise you cannot get current location."),
+            actions: <Widget>[
+              MaterialButton(
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  await PermissionHandler().openAppSettings();
+                },
+                child: Text("Open App-Settings"),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("Cancel"),
+              ),
+            ],
+          );
+        });
+  }
+}
---
+++ flutter_kids_spiel/lib/map.dart
@@ -0,0 +1,113 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'config.dart';
+import 'domain/grounds.dart';
+import 'domain/latlng_bounds.dart' as llb;
+import 'domain/peek_size.dart';
+import 'navi/navi.dart';
+import 'service/gateway.dart';
+
+class MapView extends StatefulWidget {
+  final MapViewState state = MapViewState();
+
+  set center(Position position) {
+    state.animateCamera(position);
+  }
+
+  @override
+  State<MapView> createState() => state;
+}
+
+class MapViewState extends State<MapView> {
+  final Completer<GoogleMapController> _mapController = Completer();
+  final Set<Marker> allMarkers = Set<Marker>();
+  BitmapDescriptor _markerIcon;
+  bool _myLocationEnabled = false;
+
+  CameraPosition _cameraPosition = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 17,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    _createMarkerImageFromAsset(context);
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _cameraPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _mapController.complete(controller);
+        },
+        markers: allMarkers,
+        onCameraIdle: _onCameraIdle,
+        myLocationEnabled: _myLocationEnabled,
+        myLocationButtonEnabled: false,
+        zoomGesturesEnabled: true,
+      ),
+    );
+  }
+
+  void animateCamera(Position position) async {
+    final GoogleMapController c = await _mapController.future;
+    c.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+      target: LatLng(position.latitude, position.longitude),
+      zoom: ZOOM.toDouble(),
+    )));
+
+    setState(() {
+      _myLocationEnabled = true;
+    });
+  }
+
+  void _onCameraIdle() async {
+    final GoogleMapController c = await _mapController.future;
+
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+
+    final PeekSize peekSize = PeekSize(width, height);
+
+    final bounds = await c.getVisibleRegion();
+    final latLngBounds = llb.LatLngBounds.from(bounds);
+
+    final Grounds grounds = await Gateway().loadGrounds(latLngBounds, peekSize);
+    _postGroundsOnMap(grounds);
+  }
+
+  _postGroundsOnMap(Grounds grounds) async {
+    setState(() {
+      allMarkers.clear();
+      grounds.data.forEach((ground) {
+        allMarkers.add(Marker(
+          visible: true,
+          markerId: MarkerId(ground.id ?? "unknown ID"),
+          position: ground.latLng,
+          icon: _markerIcon,
+          onTap: () {
+            INavi.build().openMap(ground.latLng);
+          },
+        ));
+      });
+    });
+  }
+
+  _createMarkerImageFromAsset(BuildContext context) async {
+    if (_markerIcon == null) {
+      final ImageConfiguration imageConfiguration =
+          createLocalImageConfiguration(context, size: Size(600, 600));
+      BitmapDescriptor.fromAssetImage(
+              imageConfiguration, "asserts/images/ic_pin.png")
+          .then((BitmapDescriptor bitmap) {
+        setState(() {
+          _markerIcon = bitmap;
+        });
+      });
+    }
+  }
+}
