95503253f77fec175e175a51147864aee41500a7
ChrisDEV <7869833+XinyueZ@users.noreply.github.com>
2019-06-02 17:48:06 +0200

    Add project flutter_kids_spiel (#10)

---
+++ flutter_kids_spiel/android/app/src/main/kotlin/io/flutter_kids_spiel/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.flutter_kids_spiel
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
