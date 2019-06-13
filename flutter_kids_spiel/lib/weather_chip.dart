a167c09e339510f8b119daac756382d3fadd3d38
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-06-13 22:18:39 +0200

    flutter_kids_spiel: WeatherChip handling event

--- flutter_kids_spiel/lib/weather_chip.dart
+++ flutter_kids_spiel/lib/weather_chip.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'config.dart';
 import 'domain/weather.dart';
 import 'map.dart';
+import 'service/gateway.dart';
 
 typedef LoadingWeatherCallback = Function(Weather weather);
 
@@ -32,28 +34,40 @@ class _WeatherChipState extends State<WeatherChip> {
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: Alignment.topLeft,
-      child: Container(
-        margin: EdgeInsets.only(top: 25.0, left: 15.0),
-        child: Chip(
-          label: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                  width: 25,
-                  height: 25,
-                  child: Image.network(
-                      _weather?.iconLocation?.toString() ?? DEFAULT_ICON_URL)),
-              Text(
-                _weather?.temperatureString ?? DATA_NULL,
-                style: TextStyle(color: Colors.white),
+        alignment: Alignment.topLeft,
+        child: Container(
+          margin: EdgeInsets.only(top: 25.0, left: 15.0),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(3),
+            onTap: () async {
+              Position position = await Geolocator()
+                  .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+              Locale myLocale = Localizations.localeOf(context);
+              final weather = await Gateway().loadWeather(position.latitude,
+                  position.longitude, myLocale.toLanguageTag());
+              debugPrint(weather.toString());
+
+              loadingWeatherCallback(weather);
+            },
+            child: Chip(
+              label: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                      width: 25,
+                      height: 25,
+                      child: Image.network(_weather?.iconLocation?.toString() ??
+                          DEFAULT_ICON_URL)),
+                  Text(
+                    _weather?.temperatureString ?? DATA_NULL,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
               ),
-            ],
+              backgroundColor: Colors.pinkAccent,
+            ),
           ),
-          backgroundColor: Colors.pinkAccent,
-        ),
-      ),
-    );
+        ));
   }
 }
