2161d00732d3257f87fe6eb3f67725b6a2df86d2
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-07-20 20:44:44 +0200

    flutter_pretrained_lang_model: Add flutter_pretrained_lang_model

---
+++ flutter_pretrained_lang_model/lib/file_picker_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'config.dart';
+
+class FilePickerWidget extends StatefulWidget {
+  FilePickerWidget({Key key}) : super(key: key);
+
+  @override
+  _FilePickerWidgetState createState() => _FilePickerWidgetState();
+}
+
+class _FilePickerWidgetState extends State<FilePickerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: MAIN_COLOR,
+          elevation: 15,
+          title: Text("MOIA INVOICE"),
+        ),
+        body: Center(
+          child: RaisedButton(
+              child: Text("Open INVOICE directory"), onPressed: () {}),
+        ),
+      ),
+    );
+  }
+}
