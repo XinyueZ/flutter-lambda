27bae588df96cf47c3b8dc646fe55df77bea46c0
ChrisDEV <7869833+XinyueZ@users.noreply.github.com>
2019-07-21 20:52:26 +0200

    Add flutter_pretrained_lang_model
    - Load directory files, use new lib (#64)
    - Build invoice list and bill list (#65)
    - Filter out invoices files (#66)

---
+++ flutter_pretrained_lang_model/lib/bill_list_widget.dart
@@ -0,0 +1,85 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+
+class BillListWidget extends StatefulWidget {
+  final Directory _billFileDirectory;
+  final List<File> _invoiceFileList;
+
+  BillListWidget(this._billFileDirectory, this._invoiceFileList);
+
+  @override
+  _BillListWidgetState createState() => _BillListWidgetState();
+}
+
+class _BillListWidgetState extends State<BillListWidget> {
+  List<FileSystemEntity> _fileList;
+
+  @override
+  void initState() {
+    _loadFilesContainBill();
+    super.initState();
+  }
+
+  _loadFilesContainBill() async {
+    setState(() {
+      final dirFiles = widget._billFileDirectory.listSync().toList();
+      _fileList = dirFiles.where((dirFile) {
+        return widget._invoiceFileList
+            .where((invoiceFile) => invoiceFile.path == dirFile.path)
+            .isNotEmpty;
+      }).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.functions,
+            color: Colors.white,
+          ),
+          onPressed: () {}),
+      appBar: AppBar(
+        title: Text("Bill List"),
+      ),
+      body: ListView.builder(
+        itemCount: _fileList == null ? 0 : _fileList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return InkResponse(
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 8, bottom: 8, right: 8),
+                      child: CircleAvatar(
+                        child: Icon(
+                          Icons.monetization_on,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(basename(_fileList[index].path),
+                            textAlign: TextAlign.left),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
