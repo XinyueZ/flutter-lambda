ecef2e2cd5aed99f24453b62d921a2a1d99f2e4b
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-07-28 20:58:22 +0200

    flutter_pretrained_lang_model: using fab(progress) to indicate filtering

--- flutter_pretrained_lang_model/lib/invoice_list_widget.dart
+++ flutter_pretrained_lang_model/lib/invoice_list_widget.dart
@@ -5,6 +5,7 @@ import 'package:open_file/open_file.dart';
 import 'package:path/path.dart';
 
 import 'bill_list_widget.dart';
+import 'config.dart';
 import 'invoice_file_detector.dart';
 
 class InvoiceListWidget extends StatefulWidget {
@@ -18,6 +19,10 @@ class InvoiceListWidget extends StatefulWidget {
 
 class _InvoiceListWidgetState extends State<InvoiceListWidget> {
   List<FileSystemEntity> _fileList;
+  bool _isFiltering = false;
+
+  Widget _fabIcon = Icon(Icons.check);
+  Widget _fabLabel = Container(height: 0.0, width: 0.0);
 
   @override
   void initState() {
@@ -31,15 +36,45 @@ class _InvoiceListWidgetState extends State<InvoiceListWidget> {
     });
   }
 
+  _updateFAB() {
+    setState(() {
+      if (_isFiltering) {
+        _fabLabel = Text(
+          "filtering...",
+          style: TextStyle(fontStyle: FontStyle.italic, color: MAIN_COLOR),
+        );
+        _fabIcon = Container(
+          margin: EdgeInsets.only(right: 5),
+          child: SizedBox(
+            width: 20,
+            height: 20,
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ),
+          ),
+        );
+      } else {
+        _fabLabel = Container(
+          width: 0,
+          height: 0,
+        );
+        _fabIcon = Icon(Icons.check);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          child: Icon(
-            Icons.check,
-            color: Colors.white,
-          ),
+      floatingActionButton: FloatingActionButton.extended(
+          label: _fabLabel,
+          icon: Padding(
+              padding: EdgeInsets.only(
+                left: 10,
+              ),
+              child: _fabIcon),
           backgroundColor: Colors.greenAccent,
+          isExtended: _isFiltering,
           onPressed: () {
             _openBillListWidget(context);
           }),
@@ -90,8 +125,19 @@ class _InvoiceListWidgetState extends State<InvoiceListWidget> {
 
   /*
    * Open the list of files which are confirmed as an invoice file.
+   * The FAB will be update to "loading" status until finishing.
    */
   _openBillListWidget(BuildContext context) async {
+    if (_isFiltering) return;
+
+    _toggleFiltering(bool filtering) {
+      setState(() {
+        _isFiltering = filtering;
+        _updateFAB();
+      });
+    }
+
+    _toggleFiltering(true);
     final listConfirmed = List<File>();
     final fileListStream = Stream.fromIterable(_fileList);
     await for (File file in fileListStream) {
@@ -101,6 +147,7 @@ class _InvoiceListWidgetState extends State<InvoiceListWidget> {
         listConfirmed.add(file);
       }
     }
+    _toggleFiltering(false);
     Navigator.of(context).push(MaterialPageRoute(builder: (_) {
       return BillListWidget(widget._invoiceFilesDirectory, listConfirmed);
     }));
