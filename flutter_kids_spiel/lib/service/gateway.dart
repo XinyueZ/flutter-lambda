e6f92740ff4b6b8172d5a01d95768a401aa9be63
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-12-01 14:59:38 +0100

    flutter_kids_spiel: refactor http-provider for all calles

--- flutter_kids_spiel/lib/service/gateway.dart
+++ flutter_kids_spiel/lib/service/gateway.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_kids_spiel/domain/ground.dart';
 import 'package:flutter_kids_spiel/domain/grounds.dart';
 import 'package:flutter_kids_spiel/domain/latlng_bounds.dart';
@@ -9,32 +8,40 @@ import 'package:flutter_kids_spiel/domain/moia/service_area.dart';
 import 'package:flutter_kids_spiel/domain/peek_size.dart';
 import 'package:flutter_kids_spiel/domain/request.dart';
 import 'package:flutter_kids_spiel/domain/weather.dart';
-import 'package:provider/provider.dart';
 import 'package:sprintf/sprintf.dart';
 
-import '../config.dart';
 import '../app_credentials.dart';
+import '../config.dart';
 import 'decoder_helper.dart';
 
 abstract class HttpProvider {
   Future<dynamic> get(path);
 
   Future<dynamic> post(path, data);
+
+  set requestHeaders(Map<String, String> keyValues);
+
+  void clearHeaders();
 }
 
 class DioProvider implements HttpProvider {
   final Dio _dio = Dio();
-  static final DioProvider instance = DioProvider();
-
-  DioProvider() {
-    _dio.options.headers = {"Content-Type": "application/json"};
-  }
 
   @override
   Future<dynamic> get(path) => _dio.get(path);
 
   @override
   Future<dynamic> post(path, data) => _dio.post(path, data: data);
+
+  @override
+  set requestHeaders(Map<String, String> keyValues) {
+    _dio.options.headers = keyValues;
+  }
+
+  @override
+  void clearHeaders() {
+    _dio.options.headers.clear();
+  }
 }
 
 class Gateway {
@@ -54,7 +61,10 @@ class Gateway {
 
   fetchGrounds(LatLngBounds bound, PeekSize peekSize) async {
     final payload = GroundRequest.from(bound, peekSize).toPayload();
+
+    _httpProvider.requestHeaders = {"Content-Type": "application/json"};
     final response = await _httpProvider.post(API_GROUNDS, payload);
+    _httpProvider.clearHeaders();
 
     final Map<String, dynamic> feedsMap =
         DecoderHelper.getJsonDecoder().convert(response.toString());
@@ -71,7 +81,10 @@ class Gateway {
   fetchWeather(double lat, double lng, String lang) async {
     final path = sprintf(API_WEATHER, [lat, lng, lang]);
 
+    _httpProvider.requestHeaders = {"Content-Type": "application/json"};
     final response = await _httpProvider.get(path);
+    _httpProvider.clearHeaders();
+
     final Map<String, dynamic> feedsMap =
         DecoderHelper.getJsonDecoder().convert(response.toString());
     weatherController.sink.add(Weather.from(feedsMap));
@@ -79,25 +92,28 @@ class Gateway {
 
   fetchMOIAServiceAreas() async {
     final String pathAuth = sprintf("%s%s", [MOIA_API_HOST, MOIA_API_AUTH]);
-    var dio = Dio();
-    dio.options.headers = {
+
+    _httpProvider.requestHeaders = {
       "Content-Type": "application/json",
       "accept": "application/vnd.moia+json",
       "authorization": MOIA_AUTHORIZATION
     };
-    var response = await dio.post(pathAuth, data: MOIA_USER);
+    var response = await _httpProvider.post(pathAuth, MOIA_USER);
+    _httpProvider.clearHeaders();
+
     var feedsMap = DecoderHelper.getJsonDecoder().convert(response.toString());
     final String idToken = feedsMap["id_token"];
 
-    dio = Dio();
-    dio.options.headers = {
+    _httpProvider.requestHeaders  = {
       "Content-Type": "application/vnd.moia.v1+json",
       "accept": "application/vnd.moia.v1+json",
       "moia-auth": idToken
     };
     final String pathServiceAreas =
         sprintf("%s%s", [MOIA_API_HOST, MOIA_API_SERVICE_AREAS]);
-    response = await dio.get(pathServiceAreas);
+    response = await _httpProvider.get(pathServiceAreas);
+    _httpProvider.clearHeaders();
+
     feedsMap = DecoderHelper.getJsonDecoder().convert(response.toString());
     moiaServiceAreasController.sink.add(MOIAServiceAreas.from(feedsMap));
   }
