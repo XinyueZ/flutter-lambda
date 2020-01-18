46e3970ca9438a6b84633da6cd601328478728a7
XinyueZ <dev.xinyue.zhao@gmail.com>
2020-01-18 15:51:50 +0100

    flutter_hacker_news: put router navi-naming to const

---
+++ flutter_hacker_news/lib/router/navigation_constants.dart
@@ -0,0 +1,3 @@
+///Root router
+const String ROOT_TO_SPLASH = "/";
+const String ROOT_TO_CONTENT = "/content";
--- flutter_hacker_news/lib/router/root_router.dart
+++ flutter_hacker_news/lib/router/root_router.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hacker_news/router/navigation_constants.dart';
 import 'package:flutter_hacker_news/widgets/hn_content_widget.dart';
 import 'package:flutter_hacker_news/widgets/hn_splash_widget.dart';
 
 Route<dynamic> generateRootRoute(RouteSettings settings) {
   switch (settings.name) {
-    case "/":
+    case ROOT_TO_SPLASH:
       return CupertinoPageRoute<void>(
         settings: settings,
         builder: (BuildContext context) => HNSplashWidget(),
       );
-    case "/content":
+    case ROOT_TO_CONTENT:
       return CupertinoPageRoute<void>(
         settings: settings,
         builder: (BuildContext context) => HNContentWidget(),
