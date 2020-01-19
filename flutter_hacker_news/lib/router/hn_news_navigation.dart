2875456bee47ce23bb21ae4ea479076db533972c
XinyueZ <dev.xinyue.zhao@gmail.com>
2020-01-19 22:16:40 +0100

    flutter_hacker_news: handling back-press to popup instead close app

--- flutter_hacker_news/lib/router/hn_news_navigation.dart
+++ flutter_hacker_news/lib/router/hn_news_navigation.dart
@@ -8,28 +8,32 @@ import 'navigation_constants.dart';
 
 class HNNewsNavigation extends CupertinoTabView {
   HNNewsNavigation()
-      : super(onGenerateRoute: (RouteSettings settings) {
-          switch (settings.name) {
-            case TAB_HN_NEWS_TO_ROOT:
-              return CupertinoPageRoute<void>(
-                settings: settings,
-                builder: (BuildContext context) => HNNewsWidget(),
-              );
-            case TAB_HN_NEWS_TO_DETAIL:
-              if (settings.arguments is! HNStory) {
-                throw Exception(
-                    "Unexpected object ${settings.arguments} for HNNewsDetailWidget.");
+      : super(
+            navigatorKey: newsTabNavKey,
+            onGenerateRoute: (RouteSettings settings) {
+              switch (settings.name) {
+                case TAB_HN_NEWS_TO_ROOT:
+                  return CupertinoPageRoute<void>(
+                    settings: settings,
+                    builder: (BuildContext context) => HNNewsWidget(),
+                  );
+                case TAB_HN_NEWS_TO_DETAIL:
+                  if (settings.arguments is! HNStory) {
+                    throw Exception(
+                        "Unexpected object ${settings.arguments} for HNNewsDetailWidget.");
+                  }
+                  final HNStory hnItem = settings.arguments as HNStory;
+                  return CupertinoPageRoute<void>(
+                    settings: settings,
+                    builder: (BuildContext context) =>
+                        HNNewsDetailWidget(item: hnItem),
+                  );
+                default:
+                  throw Exception(
+                      "Unexpected route ${settings.name} in HNNewsNavigation.");
               }
-              final HNStory hnItem = settings.arguments as HNStory;
-              return CupertinoPageRoute<void>(
-                settings: settings,
-                builder: (BuildContext context) => HNNewsDetailWidget(item: hnItem),
-              );
-            default:
-              throw Exception(
-                  "Unexpected route ${settings.name} in HNNewsNavigation.");
-          }
-        });
+            });
 
   static const int TAB_HN_NEWS_INDEX = 0;
+  static GlobalKey<NavigatorState> newsTabNavKey = GlobalKey<NavigatorState>();
 }
--- flutter_hacker_news/lib/router/root_tab_navigation.dart
+++ flutter_hacker_news/lib/router/root_tab_navigation.dart
@@ -6,50 +6,68 @@ import 'hn_jobs_navigation.dart';
 import 'hn_news_navigation.dart';
 
 class RootTabNavigation extends StatelessWidget {
+  GlobalKey<NavigatorState> currentNavigatorKey() {
+    switch (_tabController.index) {
+      case HNNewsNavigation.TAB_HN_NEWS_INDEX:
+        return HNNewsNavigation.newsTabNavKey;
+        break;
+    }
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: CupertinoTabScaffold(
-          tabBar: CupertinoTabBar(
-            backgroundColor: APP_PRIMARY_COLOR,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.fiber_new,
-                  color: Colors.white,
+      child: WillPopScope(
+        onWillPop: () async {
+          return !await currentNavigatorKey().currentState.maybePop();
+        },
+        child: CupertinoTabScaffold(
+            controller: _tabController,
+            tabBar: CupertinoTabBar(
+              backgroundColor: APP_PRIMARY_COLOR,
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.fiber_new,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    "NEWS",
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
-                title: Text(
-                  "NEWS",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.work,
-                  color: Colors.white,
-                ),
-                title: Text(
-                  "JOBS",
-                  style: TextStyle(color: Colors.white),
-                ),
-              )
-            ],
-          ),
-          tabBuilder: (BuildContext context, int index) {
-            CupertinoTabView featureNavigation;
-            switch (index) {
-              case HNNewsNavigation.TAB_HN_NEWS_INDEX:
-                featureNavigation = HNNewsNavigation();
-                break;
-              case HNJobsNavigation.TAB_HN_JOBS_INDEX:
-                featureNavigation = HNJobsNavigation();
-                break;
-              default:
-                throw Exception(
-                    "Unexpected feature navigation at tab: $index in RootTabNavigation.}");
-            }
-            return featureNavigation;
-          }),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.work,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    "JOBS",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+            tabBuilder: (BuildContext context, int index) {
+              CupertinoTabView featureNavigation;
+              switch (index) {
+                case HNNewsNavigation.TAB_HN_NEWS_INDEX:
+                  featureNavigation = HNNewsNavigation();
+                  break;
+                case HNJobsNavigation.TAB_HN_JOBS_INDEX:
+                  featureNavigation = HNJobsNavigation();
+                  break;
+                default:
+                  throw Exception(
+                      "Unexpected feature navigation at tab: $index in RootTabNavigation.}");
+              }
+              return featureNavigation;
+            }),
+      ),
     );
   }
+
+  final CupertinoTabController _tabController = CupertinoTabController();
 }
